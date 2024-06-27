296ea943402f32e697a016e54bcbc59508db4543
浪子 <jkjoy@live.cn>
2024-06-27 15:37:29 +0800

    正确获取昵称

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
  <div class="author--card">
             <img src="<?php $this->options->logoUrl() ?>" class="avatar" height="64" width="64" decoding="async">
             <div class="author--name">
-                <?php $this->options->author() ?>
+                <?php $this->author(); ?>
             </div>
             <div class="author--description">
                 <?php $this->options->description() ?>
