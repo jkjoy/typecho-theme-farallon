4ab2a62d993120f111af628105202587004fdd2f
浪子 <jkjoy@live.cn>
2024-07-06 09:08:32 +0800

    none

--- postlist.php
+++ postlist.php
@@ -4,8 +4,7 @@
     <div class="content">
         <h2 class="post--title">
 			 <a href="<?php $this->permalink() ?>">
-             <!--三天内显示火苗-->
-                <?php $this->title() ?>
+             <?php $this->title() ?>
                 <?php if((time() - $this->created) < 60*60*24*3): ?>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     class="icon--sticky" stroke="currentColor" stroke-width="2" stroke-linecap="round"
