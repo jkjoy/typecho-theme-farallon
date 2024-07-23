b2bb437f59a110414be2e04e5694082f31f4cf10
浪子 <jkjoy@live.cn>
2024-07-23 12:43:33 +0800

    0.5.7

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
             'date'      =>  _t('在<span> %s </span>发布的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); ?><?php if ($this->is('post')) $this->category(',', false);?><?php if ($this->is('post')) echo ' - ';?><?php $this->options->title(); ?><?php if ($this->is('index')) echo ' - '; ?><?php if ($this->is('index')) $this->options->description() ?></title>
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('/dist/css/style.css'); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('/dist/css/style.min.css'); ?>">
     <?php if ($this->options->icoUrl): ?>
     <link rel='icon' href='<?php $this->options->icoUrl() ?>' type='image/x-icon' />
     <?php endif; ?>
