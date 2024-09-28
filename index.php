b0b20298b51d27692147aa510a5d3557b68b74ad
浪子 <jkjoy@live.cn>
2024-09-28 23:51:56 +0800

    0.6.1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * 移植自HUGO主题 farallon 原作者 bigfa  
  * @package Farallon
  * @author  老孙 
- * @version 0.6.0
+ * @version 0.6.1
  * @link https://www.imsun.org
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
