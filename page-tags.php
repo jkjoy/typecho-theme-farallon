cd8738f10e9f3d85a487ff0d60d84e51b289a0cc
浪子 <jkjoy@live.cn>
2025-03-15 09:00:28 +0800

    0.6.3
    fix

--- page-tags.php
+++ page-tags.php
@@ -12,18 +12,20 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
         <h2 class="post--single__subtitle"><?php $this->content(); ?> </h2>
     </header>
     <div class="post-content">
-                        <?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0')->to($tags); ?>
-                        <?php if($tags->have()): ?>
-                            <div class="archive--tagList">
-                                <?php while ($tags->next()): ?>
-                                    <span class="archive--tagItem">
-                                        <a role="listitem" target="<?php $this->options->sidebarLinkOpen(); ?>" data-toggle="tooltip" data-placement="top" href="<?php $tags->permalink(); ?>" rel="tag" title="<?php $tags->count(); ?> 篇文章"><?php $tags->name(); ?> (<?php $tags->count(); ?>)</a>
-                                        </span>
-                                <?php endwhile; ?>
-                            </div>
-                        <?php else: ?>
-                            <p class="text-center pb-2"><?php _e('没有任何标签'); ?></p>
-                        <?php endif; ?>
-                    </div>
+        <?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0')->to($tags); ?>
+        <?php if($tags->have()): ?>
+        <div class="archive--tagList">
+            <?php while ($tags->next()): ?>
+            <span class="archive--tagItem">
+                <a role="listitem" target="<?php $this->options->sidebarLinkOpen(); ?>" data-toggle="tooltip" data-placement="top" href="<?php $tags->permalink(); ?>" rel="tag" title="<?php $tags->count(); ?> 篇文章">
+                    <?php $tags->name(); ?> (<?php $tags->count(); ?>)
+                </a>
+            </span>
+        <?php endwhile; ?>
+        </div>
+        <?php else: ?>
+        <p class="text-center pb-2"><?php _e('没有任何标签'); ?></p>
+        <?php endif; ?>
+    </div>
 </section>
 <?php $this->need('footer.php'); ?>
